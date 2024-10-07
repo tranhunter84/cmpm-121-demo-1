1ea9fbc4a6a2753e29c2e97732113cf9f29db7b5
tranhunter84 <tranhunter84@gmail.com>
2024-10-07 16:36:45 -0700

    Step 1 completed

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,13 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Basic button that shows number of times it's been clicked
+let num_clicks = 0;
+const button = document.createElement("button");
+button.innerHTML = "Click me! 🎃";
+button.onclick = () => {
+    num_clicks += 1;
+    button.innerHTML = num_clicks + ' clicks so far 🎃';
+};
+app.append(button);
