74c2f942df4f91570e39fe7e76da59ceaa64efb6
tranhunter84 <tranhunter84@gmail.com>
2024-10-08 01:31:22 -0700

    Step 5 completed

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,9 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Counter variable
+// Counter and growth rate variables
 let counter: number = 0;
+let growthRate: number = 0; 
 
 // Div that reports the counter's value
 const reportedCount = document.createElement("div");
@@ -22,9 +23,36 @@ const button = document.createElement("button");
 button.innerHTML = "BOO! Click me! 🎃";
 button.onclick = () => {
     counter += 1;
-    reportedCount.innerHTML = counter + " Jack-o'-Lanterns!"; // Update the div with the new counter value
+    updateReportedCount(); // Update the div with the new counter value
 };
 app.append(button);
+
+// Upgrade button for increasing growth rate
+const upgradeButton = document.createElement("button");
+upgradeButton.innerHTML = "Purchase Growth Rate Upgrade (Cost: 10 Jack-o'-Lanterns)";
+upgradeButton.disabled = true; 
+upgradeButton.onclick = () => {
+    if (counter >= 10) {
+        counter -= 10; 
+        growthRate += 1; 
+        updateReportedCount(); 
+        updateUpgradeButton(); 
+    }
+};
+app.append(upgradeButton);
+
+function updateReportedCount() {
+    reportedCount.innerHTML = counter.toFixed(2) + " Jack-o'-Lanterns!";
+}
+
+function updateUpgradeButton() {
+    if (counter < 10) {
+        upgradeButton.disabled = true;
+    }
+    else {
+        upgradeButton.disabled = false;
+    }
+}
 
 // Update counter based on animation frame
 // Counter increments by fractional amount (1 sec -> 1 unit)
@@ -32,10 +60,11 @@ let timeStamp: number = 0;
 function updateCounter(timestamp: number) {
     if (timeStamp) {
         const elapsed = timestamp - timeStamp;
-        counter += elapsed / 1000; 
-        reportedCount.innerHTML = counter.toFixed(2) + " Jack-o'-Lanterns!"; 
+        counter += (growthRate * elapsed) / 1000;
+        updateReportedCount();
+        updateUpgradeButton(); 
     }
     timeStamp = timestamp;
-    requestAnimationFrame(updateCounter); 
+    requestAnimationFrame(updateCounter); // Loop
 }
 requestAnimationFrame(updateCounter);
