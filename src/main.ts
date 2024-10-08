ac448944d404af1bb3fb9573383ce1723905d282
tranhunter84 <tranhunter84@gmail.com>
2024-10-07 18:46:21 -0700

    Step 2 completed

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,26 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Hunter's amazing game";
+const gameName = "Hunter's amazing game!!";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Basic button that shows number of times it's been clicked
-let num_clicks = 0;
+// Counter variable
+let counter : number = 0;
+
+// Div that reports the counter's value
+const reportedCount = document.createElement("div");
+reportedCount.innerHTML = counter + " Jack-o'-Lanterns!";
+app.append(reportedCount);
+
+// Basic button that increases the counter for every click
 const button = document.createElement("button");
-button.innerHTML = "Click me! 🎃";
+button.innerHTML = "BOO! Click me! 🎃";
 button.onclick = () => {
-    num_clicks += 1;
-    button.innerHTML = num_clicks + ' clicks so far 🎃';
+    counter += 1;
+    reportedCount.innerHTML = counter + " Jack-o'-Lanterns!"; // Update the div with the new counter value
 };
 app.append(button);
